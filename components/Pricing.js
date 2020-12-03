a728057075f2e6f1649b77f7335a696fa4f607f7
Angus McLauchlan <angusmclauchlan@Anguss-MacBook-Pro.local>
2020-12-03 23:38:57 +1000

    Pricing text update

--- components/Pricing.js
+++ components/Pricing.js
@@ -39,11 +39,9 @@ export default function Pricing({ products }) {
       <div className="max-w-6xl mx-auto py-8 sm:py-24 px-4 sm:px-6 lg:px-8">
         <div className="sm:flex sm:flex-col sm:align-center">
           <h1 className="text-4xl font-extrabold text-white sm:text-center sm:text-6xl">
-            Pricing Plans
-          </h1>
+          Which type of client are you?</h1>
           <p className="mt-5 text-xl text-accents-6 sm:text-center sm:text-2xl max-w-2xl m-auto">
-            Start building for free, then add a site plan to go live. Account
-            plans unlock additional features.
+          Choose Pay On Demand or a Subscription.
           </p>
           <div className="relative self-center mt-6 bg-primary-2 rounded-lg p-0.5 flex sm:mt-8 border border-accents-0">
             <button
